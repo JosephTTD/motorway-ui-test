6196b17bd238fea4955df1e94552691f3c8bbfee
JosephTTD <20605906+JosephTTD@users.noreply.github.com>
2023-04-05 14:35:43 +0100

    Update picture.js

--- src/components/picture.js
+++ src/components/picture.js
@@ -2,30 +2,29 @@ import LazyLoad from "react-lazyload";
 import { useState } from "react";
 
 const Picture = ({ imgUrl, imgDesc, classes, style }) => {
-    const [loaded, setLoaded] = useState(false);
-  
-    return (
-      <picture className={`${classes} ${loaded ? 'image-after' : 'image-before'}`}>
-        <LazyLoad once>
-          <source
-            srcSet={`${imgUrl}.webp`}
-            type="image/webp"
-            className={classes}
-            style={style}
-          />
-        </LazyLoad>
-        <LazyLoad once>
-          <img
-            src={`${imgUrl}.jpg`}
-            alt={imgDesc ?? "An image"}
-            className={`${classes} ${loaded ? 'image-after' : 'image-before'}`}
-            rel="prefetch"
-            style={style}
-            onLoad={() => setLoaded(true)}
-          />
-        </LazyLoad>
-      </picture>
-    );
+  const [loaded, setLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setLoaded(true);
   };
-  
-  export default Picture;
+
+  return (
+    <picture className={classes} style={style}>
+      <LazyLoad once>
+        <source srcSet={`${imgUrl}.webp`} type="image/webp" />
+      </LazyLoad>
+      <LazyLoad once>
+        <img
+          src={`${imgUrl}.jpg`}
+          alt={imgDesc ?? "An image"}
+          className={classes}
+          style={{ ...style, opacity: loaded ? 1 : 0, transition: "opacity 0.5s ease-in-out" }}
+          rel="prefetch"
+          onLoad={handleImageLoad}
+        />
+      </LazyLoad>
+    </picture>
+  );
+};
+
+export default Picture;
